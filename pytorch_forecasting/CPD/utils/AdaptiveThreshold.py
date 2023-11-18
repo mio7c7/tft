51a9d6d83ed01349c3cf3e8144e44396390e4c32
s39 <mio7c7@gmail.com>
2023-11-18 12:52:19 +1100

    rmit

--- pytorch_forecasting/CPD/utils/AdaptiveThreshold.py
+++ pytorch_forecasting/CPD/utils/AdaptiveThreshold.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pylab
+from math import *
+
 
 def thresholding_algo(y, lag, threshold, influence):
     signals = np.zeros(len(y))
@@ -28,35 +30,130 @@ def thresholding_algo(y, lag, threshold, influence):
                 avgFilter = np.asarray(avgFilter),
                 stdFilter = np.asarray(stdFilter))
 
+class HDDM_A():
+    def __init__(self, drift_confidence=0.001, warning_confidence=0.005):
+        self.n_min = 0
+        self.c_min = 0
+        self.total_n = 0
+        self.total_c = 0
+        self.n_max = 0
+        self.c_max = 0
+        self.n_estimation = 0
+        self.c_estimation = 0
+        self.in_concept_change = None
+        self.in_warning_zone = None
+        self.estimation = None
+        self.delay = None
+
+        self.drift_confidence = drift_confidence #alpha
+        self.warning_confidence = warning_confidence
+
+    def add_element(self, error):
+        self.total_n += 1
+        self.total_c += error
+        if self.n_min == 0:
+            self.n_min = self.total_n
+            self.c_min = self.total_c
+        if self.n_max == 0:
+            self.n_max = self.total_n
+            self.c_max = self.total_c
+
+        cota = sqrt(1.0 / (2 * self.n_min) * log(1.0 / self.drift_confidence))
+        cota1 = sqrt(1.0 / (2 * self.total_n) * log(1.0 / self.drift_confidence))
+
+        if self.c_min / self.n_min + cota >= self.total_c / self.total_n + cota1:
+            self.c_min = self.total_c
+            self.n_min = self.total_n
+
+        cota = sqrt(1.0 / (2 * self.n_max) * log(1.0 / self.drift_confidence))
+        if self.c_max / self.n_max - cota <= self.total_c / self.total_n - cota1:
+            self.c_max = self.total_c
+            self.n_max = self.total_n
+
+        if self._mean_incr(self.c_min, self.n_min, self.total_c, self.total_n, self.drift_confidence):
+            self.n_estimation = self.total_n - self.n_min
+            self.c_estimation = self.total_c - self.c_min
+            self.n_min = self.n_max = self.total_n = 0
+            self.c_min = self.c_max = self.total_c = 0
+            self.in_concept_change = True
+            self.in_warning_zone = False
+        elif self._mean_incr(self.c_min, self.n_min, self.total_c, self.total_n, self.warning_confidence):
+            self.in_concept_change = False
+            self.in_warning_zone = True
+        else:
+            self.in_concept_change = False
+            self.in_warning_zone = False
+        self._update_estimations()
+
+    def _mean_incr(self, c_min, n_min, total_c, total_n, confidence):
+        if n_min == total_n:
+            return False
+
+        m = (total_n - n_min) / n_min * (1.0 / total_n)
+        cota = sqrt(m / 2 * log(2.0 / confidence))
+        return total_c / total_n - c_min / n_min >= cota
+
+    def _update_estimations(self):
+        """ update_estimations
+
+        Update the length estimation and delay.
+
+        """
+        if self.total_n >= self.n_estimation:
+            self.c_estimation = self.n_estimation = 0
+            self.estimation = self.total_c / self.total_n
+            self.delay = self.total_n
+        else:
+            self.estimation = self.c_estimation / self.n_estimation
+            self.delay = self.n_estimation
+
+    def detected_change(self):
+        return self.in_concept_change
+
+    def detected_warning_zone(self):
+        return self.in_warning_zone
+
 
 if __name__ == '__main__':
-    y = np.array(
-        [1, 1, 1.1, 1, 0.9, 1, 1, 1.1, 1, 0.9, 1, 1.1, 1, 1, 0.9, 1, 1, 1.1, 1, 1, 1, 1, 1.1, 0.9, 1, 1.1, 1, 1, 0.9,
-         1, 1.1, 1, 1, 1.1, 1, 0.8, 0.9, 1, 1.2, 0.9, 1, 1, 1.1, 1.2, 1, 1.5, 1, 3, 2, 5, 3, 2, 1, 1, 1, 0.9, 1, 1, 3,
-         2.6, 4, 3, 3.2, 2, 1, 1, 0.8, 4, 4, 2, 2.5, 1, 1, 1])
+    hddm_a = HDDM_A()
+    data_stream = np.random.randint(2, size=2000)
+    for i in range(999, 1500):
+        data_stream[i] = 0
+    for i in range(2000):
+        hddm_a.add_element(data_stream[i])
+        if hddm_a.detected_warning_zone():
+            print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
+        if hddm_a.detected_change():
+            print('Change has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
 
-    # Settings: lag = 30, threshold = 5, influence = 0
-    lag = 30
-    threshold = 5
-    influence = 0
 
-    # Run algo with settings from above
-    result = thresholding_algo(y, lag=lag, threshold=threshold, influence=influence)
-
-    # Plot result
-    pylab.subplot(211)
-    pylab.plot(np.arange(1, len(y) + 1), y)
-
-    pylab.plot(np.arange(1, len(y) + 1),
-               result["avgFilter"], color="cyan", lw=2)
-
-    pylab.plot(np.arange(1, len(y) + 1),
-               result["avgFilter"] + threshold * result["stdFilter"], color="green", lw=2)
-
-    pylab.plot(np.arange(1, len(y) + 1),
-               result["avgFilter"] - threshold * result["stdFilter"], color="green", lw=2)
-
-    pylab.subplot(212)
-    pylab.step(np.arange(1, len(y) + 1), result["signals"], color="red", lw=2)
-    pylab.ylim(-1.5, 1.5)
-    pylab.show()
+    # y = np.array(
+    #     [1, 1, 1.1, 1, 0.9, 1, 1, 1.1, 1, 0.9, 1, 1.1, 1, 1, 0.9, 1, 1, 1.1, 1, 1, 1, 1, 1.1, 0.9, 1, 1.1, 1, 1, 0.9,
+    #      1, 1.1, 1, 1, 1.1, 1, 0.8, 0.9, 1, 1.2, 0.9, 1, 1, 1.1, 1.2, 1, 1.5, 1, 3, 2, 5, 3, 2, 1, 1, 1, 0.9, 1, 1, 3,
+    #      2.6, 4, 3, 3.2, 2, 1, 1, 0.8, 4, 4, 2, 2.5, 1, 1, 1])
+    #
+    # # Settings: lag = 30, threshold = 5, influence = 0
+    # lag = 30
+    # threshold = 5
+    # influence = 0
+    #
+    # # Run algo with settings from above
+    # result = thresholding_algo(y, lag=lag, threshold=threshold, influence=influence)
+    #
+    # # Plot result
+    # pylab.subplot(211)
+    # pylab.plot(np.arange(1, len(y) + 1), y)
+    #
+    # pylab.plot(np.arange(1, len(y) + 1),
+    #            result["avgFilter"], color="cyan", lw=2)
+    #
+    # pylab.plot(np.arange(1, len(y) + 1),
+    #            result["avgFilter"] + threshold * result["stdFilter"], color="green", lw=2)
+    #
+    # pylab.plot(np.arange(1, len(y) + 1),
+    #            result["avgFilter"] - threshold * result["stdFilter"], color="green", lw=2)
+    #
+    # pylab.subplot(212)
+    # pylab.step(np.arange(1, len(y) + 1), result["signals"], color="red", lw=2)
+    # pylab.ylim(-1.5, 1.5)
+    # pylab.show()
